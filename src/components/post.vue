0d52fe7ed6695df8d1f77f7808b3edf6f4b92f0e
MichaelGit202 <95455862+MichaelGit202@users.noreply.github.com>
2025-01-22 12:32:56 -0500

    some changes 2

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,11 +1,23 @@
 <template>
-
-
-
-
-</template>
-
-<script>
-
-
-</script>
+    <div>
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyComponent',
+    data() {
+      return {
+        message: 'Hello, Vue!'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
+  
