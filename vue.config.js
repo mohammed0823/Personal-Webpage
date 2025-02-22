072e2e4a4d587a3b61283fe6987c47caef508dfa
mohammed0823 <mia5663@psu.edu>
2025-02-22 14:23:26 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Personal-Webpage-DRAFT-/' : '/'
 };
 
